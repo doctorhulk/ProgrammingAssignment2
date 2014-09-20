14f5610d92eff10df907d5460c35dc855d74af06
Doctor Hulk <piotr@aquatek.pl>
2014-09-20 22:04:24 +0200

    Assignment 2 - completed version 1.0

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,66 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Below is a pair of functions, which make caching the inverse of 
+## a matrix easy. First you need to create a "matrix" object by calling
+## the makeCacheMatrix function. Matrix elemens can be passed to the object 
+## during creaton (as an argument of the function), or later by calling the set 
+## function of the object. To prevent unnecessary computations, the inverse 
+## should be always accessed by calling the cacheSolve function.
+## 
+## Example:
+##
+## > source("cachematrix.R")
+## > mx<-makeCacheMatrix(matrix(c(1,-1,1,2),2,2))
+## > cacheSolve(mx)
+##           [,1]       [,2]
+## [1,] 0.6666667 -0.3333333
+## [2,] 0.3333333  0.3333333
+## > cacheSolve(mx)
+## MSG: Cached data.
+##           [,1]       [,2]
+## [1,] 0.6666667 -0.3333333
+## [2,] 0.3333333  0.3333333
+## mx$set(matrix(c(2,2,3,2),2,2))
+## > cacheSolve(mx)
+##      [,1] [,2]
+## [1,]   -1  1.5
+## [2,]    1 -1.0
+## > cacheSolve(mx)
+## MSG: Cached data.
+##      [,1] [,2]
+## [1,]   -1  1.5
+## [2,]    1 -1.0
 
-## Write a short comment describing this function
+## The function makeCacheMatrix creates an object, which 
+## caches a matrix. It's also able to cache the inverse
+## of the matrix. Each time the cached matrix changes, 
+## its cached inverse is invalidated. The returned object is a
+## list of methods. The matrix supplied must always be invertible.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	s<-NULL
+	set <- function(y) {
+		x<<-y
+		s<<-NULL
+	}
+	get <- function() x
+	setsolve <- function(solve) s<<-solve
+	getsolve <- function() s
+	list(get=get, set=set, setsolve=setsolve, getsolve=getsolve)
 }
 
 
-## Write a short comment describing this function
+## The function cacheSolve computes the inverse of the matrix cached
+## in the object returned by the makeCacheMatrix function. The inverse
+## is saved inside the object. If the inverse has aready been cached, 
+## it's not compted again but retrieved from the cache. 
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x) {
+	s<-x$getsolve()
+	if(!is.null(s)) {
+		message("MSG: Cached data.")
+		return(s)
+	}
+	s<-solve(x$get())
+	x$setsolve(s)
+	s
 }
+
